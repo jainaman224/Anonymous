206c484b239cfcd69f179982216ae072c2298f87
ankur <ankurj630@gmail.com>
2017-02-11 04:42:09 +0530

    channels added

--- realtime/consumers.py
+++ realtime/consumers.py
@@ -1,11 +1,22 @@
 from channels import Group
 from channels.sessions import channel_session
+from simplejson import dumps
+
+from realtime.models import Session, User
+
+
+def ws_connect(message):
+    room_id = message['path'].strip('/').split('/')[0]
+    message.reply_channel.send({"accept": True})
+    Group('chat-%s' % room_id).add(message.reply_channel)
 
 
 def websocket_receive(message):
+    room_id = message['path'].strip('/').split('/')[0]
     text = message.content.get('text')
-    print text
-    if text:
-        Group('chat-1').add(message.reply_channel)
-        Group('chat-1').send({"text": text})
-        # message.reply_channel.send({"text": "You said: {}".format(text)})
+    Group('chat-%s' % room_id).send({'text': text})
+
+
+def ws_disconnect(message):
+    room_id = message['path'].strip('/').split('/')[0]
+    Group("chat-%s" % room_id).discard(message.reply_channel)
--- realtime/routing.py
+++ realtime/routing.py
@@ -1,7 +1,9 @@
 # routing.py
 from channels.routing import route
-from .consumers import websocket_receive
+from .consumers import websocket_receive, ws_connect, ws_disconnect
 
-channel_routing = {
-    'websocket.receive': websocket_receive,
-}
+channel_routing = [
+    route('websocket.connect', ws_connect),
+    route('websocket.receive', websocket_receive),
+    route("websocket.disconnect", ws_disconnect),
+]
